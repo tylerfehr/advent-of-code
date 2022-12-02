764ea41435c250819141210e43e16ee08caa8504
Tyler Fehr <tfehr@sensourceinc.com>
2022-12-02 11:30:42 -0500

    add day 2 part 1

--- src/day-2/common.ts
+++ src/day-2/common.ts
@@ -1,5 +1,62 @@
-export const DAY_TWO_INPUT_FILE_PATH = './inputs/day-2/part-1.txt';
+import { readTextFile } from '../readfile';
 
-export const RPS_LOOKUP = {
+export const DAY_TWO_INPUT_FILE_PATH = <const> './inputs/day-2/part-1.txt';
 
+export const tournamentRounds = <[OpponentResponse, MyResponse][]> readTextFile(DAY_TWO_INPUT_FILE_PATH)
+  .trim()
+  .split('\n')
+  .map((round) => round.split(' '));
+
+/**
+ * Opponent's (elf's) choice for RPS
+ */
+export enum OpponentResponse {
+  Rock = 'A',
+  Paper = 'B',
+  Scissors = 'C'
+}
+
+/**
+ * Player's choice for RPS
+ */
+export enum MyResponse {
+  Rock = 'X',
+  Paper = 'Y',
+  Scissors = 'Z',
+}
+
+/**
+ * Maps my response to its score
+ */
+export const SCORE_LOOKUP = <const> {
+  [MyResponse.Rock]: 1,
+  [MyResponse.Paper]: 2,
+  [MyResponse.Scissors]: 3,
+}
+
+/**
+ * Outcome for a round of RPS
+ */
+export enum Outcome {
+  Loss = 0,
+  Draw = 3,
+  Win = 6,
+}
+
+/**
+ * Maps my choices to the same value for the opponent's choices
+ */
+export const SAME_CHOICE_LOOKUP = <const> {
+  [MyResponse.Rock]: OpponentResponse.Rock,
+  [MyResponse.Paper]: OpponentResponse.Paper,
+  [MyResponse.Scissors]: OpponentResponse.Scissors,
+}
+
+/**
+ * Maps my choices to the value they win over
+ */
+export const WINNING_MATCHUPS: Record<MyResponse, OpponentResponse> = <const> {
+  [MyResponse.Rock]: OpponentResponse.Scissors,
+  [MyResponse.Paper]: OpponentResponse.Rock,
+  [MyResponse.Scissors]: OpponentResponse.Paper,
 }
---
+++ src/day-2/part-1.ts
@@ -0,0 +1,26 @@
+import {
+  tournamentRounds,
+  Outcome,
+  SCORE_LOOKUP,
+  SAME_CHOICE_LOOKUP,
+  WINNING_MATCHUPS,
+} from './common';
+
+const finalScore = tournamentRounds.reduce<number>(
+  (acc, [opponentChoice, myChoice]) => {
+    const scoreForChoice = SCORE_LOOKUP[myChoice];
+
+    if (WINNING_MATCHUPS[myChoice] === opponentChoice) {
+      return acc + scoreForChoice + Outcome.Win;
+    }
+
+    if (SAME_CHOICE_LOOKUP[myChoice] === opponentChoice) {
+      return acc + scoreForChoice + Outcome.Draw;
+    }
+
+    return acc + scoreForChoice + Outcome.Loss;
+  },
+  0,
+);
+
+console.log(`The final score following the strategy guide is ${finalScore}`);
