6e3f0e6b8c3c6efaa6845aa867e506f458373169
Tyler Fehr <tfehr@proton.me>
2022-12-04 10:32:06 -0500

    complete d4p1

--- src/day-4/common.ts
+++ src/day-4/common.ts
@@ -2,14 +2,16 @@ import { readTextFile } from "../readfile";
 
 export const DAY_FOUR_INPUT_FILE_PATH = <const> './inputs/day-4/part-1.txt';
 
-export const getSectionAssignments = readTextFile(DAY_FOUR_INPUT_FILE_PATH)
+export const sectionAssignments: [Pair, Pair][] = readTextFile(DAY_FOUR_INPUT_FILE_PATH)
   .split('\n')
-  .map<[pairOne: [number, number], PairTwo: [number, number]]>((row) => {
+  .map<[pairOne: Pair, pairTwo: Pair]>((row) => {
       const [assignmentPairOne, assignmentPairTwo] = row.split(',');
 
-      const pairOneArr = <[number, number]> assignmentPairOne.split('-').map((p) => +p);
-      const pairTwoArr = <[number, number]> assignmentPairTwo.split('-').map((p) => +p);
+      const pairOne = <Pair> assignmentPairOne.split('-').map((p) => +p);
+      const pairTwo = <Pair> assignmentPairTwo.split('-').map((p) => +p);
 
-      return [pairOneArr, pairTwoArr]
+      return [pairOne, pairTwo]
     },
   );
+
+export type Pair = [number, number];
