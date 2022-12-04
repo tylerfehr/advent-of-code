2cc0237e07e83ab8fb19e6984540607aa6f9c255
Tyler Fehr <tfehr@proton.me>
2022-12-04 16:14:59 -0500

    fix spacing

--- src/day-4/part-2.ts
+++ src/day-4/part-2.ts
@@ -1,10 +1,8 @@
 import { isFullyOverlapping, sectionAssignments, type Pair } from "./common";
 
-const isPartiallyOverlapping = ([o1, c1]: Pair, [o2, c2]: Pair): boolean => (
-  (c1 >= o2 && c1 <= c2)
+const isPartiallyOverlapping = ([o1, c1]: Pair, [o2, c2]: Pair): boolean => (c1 >= o2 && c1 <= c2)
   || (c2 >= o1 && c2 <= c1)
-  || isFullyOverlapping([o1, c1], [o2, c2])
-);
+  || isFullyOverlapping([o1, c1], [o2, c2]);
 
 const numOverlappingPairs = sectionAssignments
   .reduce<number>((acc, [p1, p2]) => acc + (isPartiallyOverlapping(p1, p2) ? 1 : 0), 0);
